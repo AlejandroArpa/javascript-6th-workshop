ef824b322390df81803fcf46452454ba97222a81
samuelacevedo9307 <samuelacevedo9307@gmail.com>
2024-04-30 17:33:05 -0500

    Get and validate info in prompt

--- hotel_reservation/index.js
+++ hotel_reservation/index.js
@@ -1,17 +1,23 @@
-
+/* 
 const url = "./data.json"; 
-let data;
-let flag = false
+
+
 
 async function cargarDatos(){
 	try {
 		const response = await fetch(url);
     	if(response.ok){
-			data = await response.json();
-			console.log('Habitaciones: ',data.rooms);
-			console.log('Tipos: ', data.roomTypes);
-			flag = true;
+			const data = await response.json();
+			const {rooms, roomTypes} = data;
+			const peopleNumber = Number (prompt(`Para cuantas personas es la reserva?`))
+			const roomsAvailables = rooms.filter((room) => {	
+				return roomTypes.find((type) => room.roomTypeId === type.id).capacity >= peopleNumber && room.availability;
+			})
+			const userInput = Number (prompt( "Ingrese el numero de habitacion a reservar: " + roomsAvailables.map((room)=>`\nRoom Number: ${room.number} (${
+              roomTypes.find((type) => type.id === room.roomTypeId).name
+            })`) ))
     	}
+		
     	else{
 			throw new Error("Error al cargar los datos");
     	}
@@ -40,4 +46,48 @@ setTimeout(cargarDatos, 1000);
 //   })
 //   .catch((error) => {
 //     console.error("Error al manejar la promesa:", error);
-//   });
+//   }); */
+
+
+// Ruta del archivo data.json
+const url = "./data.json"; // Cambiar por la ruta correcta
+
+// Función para cargar y mostrar el contenido de data.json
+function cargarYMostrarData() {
+  // Retorna una nueva promesa que se resuelve después del setTimeout
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // Realiza la solicitud fetch dentro del setTimeout
+      fetch(url)
+        .then((pepito) => {
+          if (!pepito.ok) {
+            throw new Error("Error al cargar los datos.");
+          }
+		  console.log(pepito);
+          return pepito.json();
+        })
+        .then((data) => {
+          console.log("Habitaciones:", data.rooms);
+          console.log("Tipos de Habitaciones:", data.roomTypes);
+          resolve(data); // Resuelve la promesa con los datos cargados
+        })
+        .catch((error) => {
+          console.error(error);
+          reject(error); // Rechaza la promesa si hay un error
+        });
+    }, 3000);
+  });
+}
+
+new Promise((resolve, reject) => {
+	
+})
+
+// Llamar a la función para cargar y mostrar el contenido de data.json
+cargarYMostrarData()
+  .then((datos)=>{
+
+  })
+  .catch((error) => {
+    console.error("Error al manejar la promesa:", error);
+  });
